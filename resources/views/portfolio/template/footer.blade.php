7d83e557099b56c01196411b7f929659591d434f
ckiefi <ra3noglaziy@gmail.com>
2020-03-01 03:08:36 +0200

    still updating

--- resources/views/portfolio/template/footer.blade.php
+++ resources/views/portfolio/template/footer.blade.php
@@ -6,3 +6,13 @@
             </div>
         </div>
     </div>
+    
+
+	<div class="footer-right">
+        
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+                
+	</div>
--- storage/framework/views/065cdb7ff222d06157f04e440ba87d70a2a1f2cb.php
+++ storage/framework/views/065cdb7ff222d06157f04e440ba87d70a2a1f2cb.php
@@ -5,4 +5,14 @@
                 <p>Copyright &copy; 2020  |  by Arslkv</p>
             </div>
         </div>
-    </div><?php /**PATH C:\xampp\htdocs\lara.vel\resources\views/portfolio\template\footer.blade.php ENDPATH**/ ?>
+    </div>
+    
+
+	<div class="footer-right">
+        
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+                
+	</div><?php /**PATH C:\xampp\htdocs\lara.vel\resources\views/portfolio\template\footer.blade.php ENDPATH**/ ?>
