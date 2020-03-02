5c69aae5bdf4b92a95f8a4a51bcc19568b723dc2
ckiefi <ra3noglaziy@gmail.com>
2020-03-02 15:39:29 +0200

    refresh footer

--- resources/views/portfolio/template/footer.blade.php
+++ resources/views/portfolio/template/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="container">
-        <div class="row center-lg center-md center-sm center-xs">
+        <div class="row left-lg left-md left-sm left-xs">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p>Copyright &copy; 2020  |  by Arslkv</p>
             </div>
@@ -11,8 +11,8 @@
 	<div class="footer-right">
         
 				<a href="https://www.instagram.com/arslkv_/"><i class="fa fa-instagram"></i></a>
-				<a href="https://ua.linkedin.com/in/svyat-voytenko-b6b7ab1a2"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="https://twitter.com/ra3noglaziy"><i class="fa fa-twitter"></i></a>
+				<a href="https://ua.linkedin.com/in/svyat-voytenko-b6b7ab1a2"><i class="fa fa-linkedin"></i></a>
                 <a href="https://github.com/ckeifi"><i class="fa fa-github"></i></a>
                 
 	</div>
--- storage/framework/views/065cdb7ff222d06157f04e440ba87d70a2a1f2cb.php
+++ storage/framework/views/065cdb7ff222d06157f04e440ba87d70a2a1f2cb.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="container">
-        <div class="row center-lg center-md center-sm center-xs">
+        <div class="row left-lg left-md left-sm left-xs">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p>Copyright &copy; 2020  |  by Arslkv</p>
             </div>
